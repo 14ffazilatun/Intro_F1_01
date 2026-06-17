18a1deea313025bea1d793cd9dbccca91ea7f66e
14ffazilatun <ffazilatun14@gmail.com>
2026-06-17 18:00:56 -0400

    Index gradient

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from "expo-router";
+import {LogBox} from "react-native";
+LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(tabs)"
+        options={{
+          headerShown: false,
+        }}
+      />
+
+      <Stack.Screen
+        name="not-found"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  );
 }
