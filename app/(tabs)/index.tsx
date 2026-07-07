2da7afbda0a9844522b235f0a0c6e755dc7d7cb5
14ffazilatun <ffazilatun14@gmail.com>
2026-07-07 16:55:11 -0400

    Added button SDK 56

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,24 +1,106 @@
-import { Image } from "expo-image"; // Will light up once used below
-import { LinearGradient } from "expo-linear-gradient"; // Will light up once used below
-import { StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
+import { LinearGradient } from "expo-linear-gradient";
+import {
+  ActivityIndicator,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+export const AppButton = (props: any) => {
+  return (
+    <Pressable
+      style={({ pressed }) => [
+        {
+          backgroundColor: props.disabled
+            ? "#ccc"
+            : pressed
+              ? "#9a3030a3"
+              : props.color || "red",
+        },
+        props.buttonStyles,
+      ]}
+      disabled={props.disabled}
+      onPress={props.onPress}
+      accessible
+      accessibilityLabel={props.accessibilityLabel || "A Button"}
+    >
+      <Text
+        style={{ fontFamily: "Michroma-Regular", fontSize: 16, color: "#fff" }}
+      >
+        {props.children || "Press Me"}
+      </Text>
+    </Pressable>
+  );
+};
+
 export default function App() {
+  // Pass the actual asset files directly via the require method
+  const [fontsLoaded] = useFonts({
+    "Michroma-Regular": require("../../assets/fonts/Michroma-Regular.ttf"),
+    "Audiowide-Regular": require("../../assets/fonts/Audiowide-Regular.ttf"),
+  });
+
+  // Block the screen layout until the font map compiles cleanly
+  if (!fontsLoaded) {
+    return (
+      <View
+        style={[
+          styles.container,
+          { backgroundColor: "#000", justifyContent: "center" },
+        ]}
+      >
+        <ActivityIndicator size="large" color="#6e1e1e" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      {/* Using the component here automatically activates the faded import */}
-      <LinearGradient colors={["#ff0000", "#000000"]} style={styles.gradient}>
-        <Text style={{ fontFamily: "Michroma", fontSize: 24, color: "#fff" }}>
+      <LinearGradient
+        colors={["#ff0000", "#6f0d0d", "#360c0c", "#000000"]}
+        style={styles.gradient}
+      >
+        <Text
+          style={{
+            fontFamily: "Michroma-Regular",
+            fontSize: 24,
+            color: "#fff",
+          }}
+        >
           Welcome to
         </Text>
         <Text
           style={{
-            fontFamily: "Audiowide",
-            fontSize: 50,
-            fontWeight: "bold",
+            fontFamily: "Audiowide-Regular",
+            fontSize: 70,
+            fontWeight: "regular",
             color: "#fff",
+            paddingVertical: 30,
           }}
         >
           THE PADDOCK CLUB
         </Text>
+        <AppButton
+          color="rgba(255,255,255,0.15)"
+          buttonStyles={{
+            fontsize: 40,
+            paddingVertical: 20,
+            paddingHorizontal: 80,
+            borderRadius: 100,
+            width: 300,
+            alignItems: "flex-end",
+            justifyContent: "center",
+            backgroundColor: "rgba(255, 255, 255, 0.06)",
+            borderWidth: 2,
+            borderColor: "rgb(161, 6, 6)",
+            backdropFilter: "blur(20px)", // works on web
+          }}
+          onPress={() => console.log("Begin")}
+        >
+          Click to begin
+        </AppButton>
       </LinearGradient>
     </View>
   );
@@ -29,7 +111,7 @@ const styles = StyleSheet.create({
     flex: 1,
     width: "100%",
     alignItems: "center",
-    justifyContent: "flex-start", // <-- puts image at top
+    justifyContent: "flex-start",
   },
   gradient: {
     flex: 1,
@@ -37,14 +119,4 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
-  // textwelcome: {
-  //   fontFamily: "Michroma",
-  //   fontWeight: "bold",
-  // },
-  // texttitle: {
-  //   fontFamily: "Audiowide",
-  //   fontSize: 30,
-  //   fontWeight: "bold",
-  //   color: "#fff",
-  // },
 });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,5 @@
 import { Stack } from "expo-router";
-<<<<<<< HEAD
-import * as SplashScreen from "expo-splash-screen";
-import { useEffect } from "react";
-import { LogBox } from "react-native";
-import { Button,Host } from '@expo/ui';
-
-export function BasicButtonExample() {
-  return (
-    <Host matchContents>
-      <Button label="Press me" onPress={() => alert('Pressed!')} />
-    </Host>
-  );
-}
-SplashScreen.preventAutoHideAsync();
-
-=======
 import {LogBox} from "react-native";
->>>>>>> parent of 5a9aa52 (added Audiowide and Michroma fonts)
 LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
